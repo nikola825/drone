ee93897fd4e9295906b9b92905cc638bc3b7a87f
nikola825 <nikola825@gmail.com>
2025-10-19 00:52:13 +0200

    seven inch PID values update

--- code/src/model/seven_inch.rs
+++ code/src/model/seven_inch.rs
@@ -4,9 +4,9 @@ pub const YAW_KP: f32 = 19.5f32;
 pub const YAW_KI: f32 = 78f32;
 pub const YAW_KD: f32 = 0.0f32;
 
-pub const PITCH_KP: f32 = 4.2;
-pub const PITCH_KI: f32 = 20.0f32;
-pub const PITCH_KD: f32 = 0.045f32;
+pub const PITCH_KP: f32 = 12.526546;
+pub const PITCH_KI: f32 = 59.650223;
+pub const PITCH_KD: f32 = 0.024790758;
 
 pub const ROLL_KP: f32 = PITCH_KP;
 pub const ROLL_KI: f32 = PITCH_KI;
